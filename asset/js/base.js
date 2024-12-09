b09ca1c2ff487ca0cb768b5f82a1927d74b25556
phamtienk4 <25a4041924@hvnh.edu.vn>
2024-12-09 11:12:35 +0700

    update

---
+++ asset/js/base.js
@@ -0,0 +1,27 @@
+var dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(item => {
+    var prev = item.previousElementSibling;
+
+    prev.addEventListener('mouseover', function () {
+        item.classList.remove('hide')
+    })
+    console.log(prev.parentElement);
+
+    prev.parentElement.addEventListener('mouseleave', function (e) {
+        item.classList.add('hide')
+    })
+})
+
+// bar responsive show
+var bar = document.querySelector('.bar')
+var dropdownBar = document.querySelector('.dropdown-bar')
+bar.addEventListener('click', function (e) {
+    e.stopPropagation()
+    dropdownBar.classList.toggle('hide')
+})
+
+// chan hien tuong noi bot
+document.addEventListener('click', function () {
+    dropdownBar.classList.add('hide')
+})
